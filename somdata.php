594738a9ad45457bce03ee14208e73dfb5be900b
Simon Walker <github@stwalkerster.co.uk>
2014-04-07 22:32:20 +0100

    hexgrid uses SOM data now

--- somdata.php
+++ somdata.php
@@ -1,37 +1,31 @@
 <?php
-// config section: -------
-$infile = "SOM.csv";
-$emptyVal = "[]";
-$sep = "  ";
-// -----------------------
+/* Design and Code Project 2014
+ *  Authors: Simon Walker
+ *  PHP script to load the topic data for the hex grids
+ *  Revision History
+ *  Initial Creation - Simon
+ *  Adjusted output format to required form - Simon
+ */
 
 // helper class
-class wrapper
-{
-    public $name = "Hexagons";
-    public $children;
-}
-
-// helper class
-class hex
-{
-    public $hex;
-    public $children;
-}
-
 class topic
 {
     public $topicId;   
     public $words;   
     public $school;
-    
-    // reference-based wrapper for constructor
-    public static function create(&$data, $key)
-    {
-        $data = new topic($data);   
-    }
-    
-    public function __construct($topic)
+    public $url;
+    public $hexX;
+    public $hexY;
+    public $hexNumber;
+
+    /**
+     * Summary of __construct
+     * @param mixed $topic Topic ID
+     * @param mixed $x X position in grid
+     * @param mixed $y Y position in grid
+     * @param mixed $t Topic number in hex
+     */
+    public function __construct($topic, $x, $y, $t)
     {
         global $topicStatement;
         
@@ -39,67 +33,70 @@ class topic
         $data = $topicStatement->fetch(PDO::FETCH_ASSOC);
         $topicStatement->closeCursor(); // IMPORTANT! Without closing the refcursor you won't free up resources or the pointer to be reused for the next execution.
         
-        $this->words = explode(" ", $data['TopicLabel']);
+        $this->words = explode(" ", trim($data['TopicLabel']));
         $this->topicId = $topic;
+        $this->hexX = $x;
+        $this->hexY = $y;
+        $this->hexNumber = $t;
+        $this->url = "topicgraphandwordle.php?topicID=" . $topic;
         $this->school = $data['OrganisationDepartment']; // FIXME
     }
 }
 
 require_once("config.inc.php");
 
-$db = new PDO("mysql:host=$dbhost;dbname=meng_rcuk;", $dbuser, $dbpass);
+$db = new PDO("mysql:host=$dbhost;dbname=$dbname;", $dbuser, $dbpass);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // disable prepared statement emulation
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
+// start a transaction so any database edits we make are invisible to other sessions.
+$db->exec("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ");
+$db->beginTransaction();
+
+// materialise the view so we can do faster lookups
+// this is only visible to our session because of the transaction, and is PK'd to our connection so we don't get blocking inserts.
+$db->exec("insert into tmp_vw_hw_topics_100 SELECT *, connection_id() FROM vw_hw_topics_100;");
+
 // prepare the statement once, execute multiple times. It's much faster that way cos you only have to send the new values to the server.
-$topicStatement = $db->prepare("SELECT TopicLabel, OrganisationDepartment FROM vw_hw_topics_100 WHERE TopicID = :id;");
+// where clause uses the entire primary key so we can use the index, but the last condition is pointless for data filtering.
+$topicStatement = $db->prepare("SELECT TopicLabel, OrganisationDepartment FROM tmp_vw_hw_topics_100 WHERE TopicID = :id AND connection = connection_id();");
 
 // read file
 $file = file_get_contents('SOM.csv');
 
-// strip lines
-$file = str_replace("\n", ",", $file);
+$topics = array();
 
-// explode on commas
-$fileData = explode(",", $file);
-
-$counter = 0;
-
-// step through the array, and poke it into shape
-array_walk($fileData, function(&$value, $key)
+$y = 0;
+foreach(explode("\n", $file) as $line)
 {
-    global $counter, $emptyVal, $sep;
-    
-    if($value == $emptyVal)
+    $x = 0;
+    foreach(explode(",", $line) as $cell)
     {
-        // empty cell
-        $value = array();
+        if($cell != "[]")
+        {
+            $t = 0;
+            foreach(explode(" ", $cell) as $topic)
+            {
+                $topics[] = new topic($topic, $x, $y, $t);
+                $t++;
+            }
+        }
+        
+        $x++;
     }
-    else
-    {
-        // non-empty cell
-        $value = explode($sep, $value);
-    }
     
-    // create the topics in-place.
-    array_walk($value, 'topic::create');
-    
-    $hex = new hex();
-    $hex->hex = $counter++;
-    $hex->children = $value;
-    
-    $value = $hex;
-});
+    $y++;
+}
 
-// wrap it as requested
-$wrapper = new wrapper();
-$wrapper->children = $fileData;
+// roll back our database edits
+$db->rollBack();
 
-header('Content-Type: application/json');
-
-echo json_encode($wrapper);
+header('Content-Type: text/javascript');
+echo "var data = ";
+echo json_encode($topics);
+echo ";";
 // end.
 die();
