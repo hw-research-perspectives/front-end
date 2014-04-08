fad3541469c7916e5adca9c6daf2851df87f1c03
Simon Walker <github@stwalkerster.co.uk>
2014-04-08 15:05:06 +0100

    topicLocations query

--- data.php
+++ data.php
@@ -19,12 +19,13 @@
 require_once("config.inc.php");
 
 // QUERY LIST HERE!
-$queryVariables = array("schoolscontribpertopic", "debug", "hwgrants", "allgrants");
+$queryVariables = array("schoolscontribpertopic", "debug", "hwgrants", "allgrants", "topicLocations");
 
 $schoolscontribpertopic = "SELECT scpt.*, dates.date1 FROM vw_schoolscontrpertopic scpt JOIN (SELECT  m1 AS date1 FROM (SELECT ((SELECT MIN(StartDate) FROM vw_schoolscontrpertopic) - INTERVAL DAYOFMONTH((SELECT  MAX(EndDate) FROM vw_schoolscontrpertopic)) - 1 DAY) + INTERVAL m MONTH AS m1 FROM (SELECT  @ROWNUM:=@ROWNUM + 1 AS m FROM (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t1, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t2, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t3, (SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4) t4, (SELECT @ROWNUM:=- 1) t0) d1) d2 WHERE m1 <= (SELECT MAX(EndDate) FROM vw_schoolscontrpertopic) ORDER BY m1) dates WHERE dates.date1 BETWEEN scpt.StartDate AND scpt.EndDate ORDER BY scpt.TopicId, dates.date1;";
 $debug = "SELECT 'sekrit' AS secretSitePassword FROM dual;"; // little easter egg... since http://is.gd/9HluJs got reverted :(
 $hwgrants = "SELECT * FROM vw_hw_grants;";
 $allgrants = "SELECT GrantRefNumber, GrantTitle, TotalGrantValue, HoldingOrganisationName FROM information;";
+$topicLocations = "select 'A34Z' as origin, CONCAT('A',location_id2 ,'Z') as destination, 10 as count from (select OrganisationPostcode  from topicmap_grants_100 tm inner join information i on i.id = tm.id where tm.TopicID=:topicID and i.OrganisationID <> '' and i.OrganisationName LIKE '%University%' group by OrganisationPostcode order by tm.Proportion desc limit 10) pc inner join location l on pc.OrganisationPostCode = l.Postcode;";
 // END OF QUERY LIST!
 
 // default to sane value
@@ -41,13 +42,18 @@ if(isset($_GET['query']))
 
 $db = new PDO("mysql:host=$dbhost;dbname=$dbname;", $dbuser, $dbpass);
 $query = $db->prepare($$queryToUse);
+if($_GET['query'] == "topicLocations")
+{
+    $query->bindValue(":topicID", $_GET['topicID']);   
+}
+
 $query->execute();
 
 // default to sane value
 $dataFormat = "debug";
 if(isset($_GET['format']))
 {
-	if(in_array($_GET['format'], array("json", "jsonfm", "tsv", "tsvfm", "debug")))
+	if(in_array($_GET['format'], array("json", "jsonfm", "tsv", "tsvfm", "debug","csv", "csvfm",)))
 	{
 		$dataFormat = $_GET['format'];
 	}
@@ -57,6 +63,11 @@ if($dataFormat == "tsv")
 {
 	header('Content-Type: text/tab-separated-values');
 	header('Content-Disposition: attachment; filename="data.tsv"');
+} 
+if($dataFormat == "csv")
+{
+	header('Content-Type: text/comma-separated-values');
+	header('Content-Disposition: attachment; filename="data.csv"');
 } 
 elseif($dataFormat == "json")
 {
@@ -90,6 +101,18 @@ if($dataFormat == "tsv" || $dataFormat == "tsvfm")
 		echo implode("\t", $row) . "\r\n";
 	}
 }
+if($dataFormat == "csv" || $dataFormat == "csvfm")
+{
+	$data = $query->fetchAll(PDO::FETCH_ASSOC);
+	
+	echo implode(",", $columnNames) . "\r\n";
+
+	foreach ($data as $row)
+	{
+		echo implode(",", $row) . "\r\n";
+	}
+}
+
 
 if($dataFormat == "json" || $dataFormat == "jsonfm")
 {
